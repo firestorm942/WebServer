41f9947cca34b01d51050ca55b85bfc2add557a3
firestorm942 <speed9425@gmail.com>
2019-07-30 19:45:50 -0500

    update project

--- src/main/java/org/fstm942/WebServer/Main.java
+++ src/main/java/org/fstm942/WebServer/Main.java
@@ -35,123 +35,123 @@ import java.net.Socket;
 import java.net.UnknownHostException;
 
 public class Main extends JavaPlugin {
-	private boolean debug;// received from config. Shows debug messages if true.
-	// ^START PROTOCOL^
-	static String closeConnection = "!Close Connection!";
-	// ^END PROTOCOL^
-	private int listeningport;// received from config
+    private boolean debug;// received from config. Shows debug messages if true.
+    // ^START PROTOCOL^
+    static String closeConnection = "!Close Connection!";
+    // ^END PROTOCOL^
+    private int listeningport;// received from config
 
-	private Main m = this;// Passed to necessary classes constructor
+    private Main m = this;// Passed to necessary classes constructor
 
-	private boolean acceptorRunning;// As long as this variable is true, the
-									// acceptor thread will keep on getting
-									// connections from the php web server
+    private boolean acceptorRunning;// As long as this variable is true, the
+    // acceptor thread will keep on getting
+    // connections from the php web server
 
-	private ServerSocket ss;
-	// private List<Integer> li = new ArrayList<Integer>();//should be used to
-	// store data from receiving thread. not used for now.
+    private ServerSocket ss;
+    // private List<Integer> li = new ArrayList<Integer>();//should be used to
+    // store data from receiving thread. not used for now.
 
-	/*
-	 * public synchronized void add(int id) {li.add(id); }//should be used to
-	 * store data from receiving thread. not used for now.
-	 * 
-	 */
+    /*
+     * public synchronized void add(int id) {li.add(id); }//should be used to
+     * store data from receiving thread. not used for now.
+     *
+     */
 
-	synchronized void logConsole(String s) {
-		if (debug)
-			getServer().getConsoleSender().sendMessage(s);
-	}
-	synchronized void logCon(String s) {//Non-debug messages
-		
-			getServer().getConsoleSender().sendMessage(s);
-	}
-	private synchronized boolean getAcceptorRunning() {
-		return acceptorRunning;
-	}
+    synchronized void logConsole(String s) {
+        if (debug)
+            getServer().getConsoleSender().sendMessage(s);
+    }
+    synchronized void logCon(String s) {//Non-debug messages
 
-	@Override
-	public void onEnable() {
-		this.saveDefaultConfig();
-		this.reloadConfig();
-		debug = getConfig().getBoolean("debug");
-		if (getConfig().isSet("listeningport")) {// Value set by user-set
-													// config.yml
-			logCon(ChatColor.GREEN + "Found a listening port for the registeration changes.");
-			try {
-				listeningport = getConfig().getInt("listeningport");
-				ss = new ServerSocket(listeningport);
-			} catch (IOException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
-		} else {// Value NOT set by user-set config.yml
-			if (getConfig().contains("listeningport")) {// defaults exists
-				logCon(
-						ChatColor.YELLOW + "Listening port for registeration changes NOT FOUND! Using default value!");
-				try {
-					listeningport = getConfig().getInt("listeningport");
-					ss = new ServerSocket(listeningport);
-				} catch (IOException e) {
-					// TODO Auto-generated catch block
-					e.printStackTrace();
-				}
-			} else {// default doesnt exist. Shutting down.
-				logCon(ChatColor.DARK_RED + "Plugin disabled! NO VALUE WAS FOUND FOR LISTENING PORT!");
-				Bukkit.getPluginManager().disablePlugin(this);
-				return;
-			}
-		}
-		// logConsole(ChatColor.ITALIC + "Started server on " +
-		// ss.getInetAddress() == null ? "NULL" : ss.getInetAddress() + ":" +
-		// ss.getLocalPort() == null ? "NULL" : ss.getLocalPort() + "!");
-		acceptorRunning = true;
-		Thread acceptor = new Thread(new Runnable() {
+        getServer().getConsoleSender().sendMessage(s);
+    }
+    private synchronized boolean getAcceptorRunning() {
+        return acceptorRunning;
+    }
 
-			@Override
-			public void run() {
-				Socket sock;
-				logConsole(ChatColor.AQUA + "-----Accepting connections-----");
+    @Override
+    public void onEnable() {
+        this.saveDefaultConfig();
+        this.reloadConfig();
+        debug = getConfig().getBoolean("debug");
+        if (getConfig().isSet("listeningport")) {// Value set by user-set
+            // config.yml
+            logCon(ChatColor.GREEN + "Found a listening port for the registeration changes.");
+            try {
+                listeningport = getConfig().getInt("listeningport");
+                ss = new ServerSocket(listeningport);
+            } catch (IOException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            }
+        } else {// Value NOT set by user-set config.yml
+            if (getConfig().contains("listeningport")) {// defaults exists
+                logCon(
+                        ChatColor.YELLOW + "Listening port for registeration changes NOT FOUND! Using default value!");
+                try {
+                    listeningport = getConfig().getInt("listeningport");
+                    ss = new ServerSocket(listeningport);
+                } catch (IOException e) {
+                    // TODO Auto-generated catch block
+                    e.printStackTrace();
+                }
+            } else {// default doesnt exist. Shutting down.
+                logCon(ChatColor.DARK_RED + "Plugin disabled! NO VALUE WAS FOUND FOR LISTENING PORT!");
+                Bukkit.getPluginManager().disablePlugin(this);
+                return;
+            }
+        }
+        // logConsole(ChatColor.ITALIC + "Started server on " +
+        // ss.getInetAddress() == null ? "NULL" : ss.getInetAddress() + ":" +
+        // ss.getLocalPort() == null ? "NULL" : ss.getLocalPort() + "!");
+        acceptorRunning = true;
+        Thread acceptor = new Thread(new Runnable() {
 
-				while (getAcceptorRunning()) {
-					try {
-						sock = ss.accept();
-						new AcceptedSocketConnection(sock, m).start();
-					} catch (IOException e) {
-						// TODO Auto-generated catch block
-						e.printStackTrace();
-					}
-				}
-				logConsole(ChatColor.LIGHT_PURPLE + "-----Done accepting connections-----");
-			}
-		});
-		acceptor.start();
-		super.onEnable();
-	}
+            @Override
+            public void run() {
+                Socket sock;
+                logConsole(ChatColor.AQUA + "-----Accepting connections-----");
 
-	@Override
-	public void onDisable() {
-		acceptorRunning = false;
-		Socket sockCloser;
-		try {
-			sockCloser = new Socket("localhost", listeningport);
-			BufferedWriter out = new BufferedWriter(new OutputStreamWriter(sockCloser.getOutputStream()));
-			out.write(Main.closeConnection);
-			out.close();
-			sockCloser.close();
-			logConsole(ChatColor.DARK_GREEN + "Closed listening web server successfully!");
-		} catch (UnknownHostException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		try {
-			ss.close();
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		super.onDisable();
-	}
+                while (getAcceptorRunning()) {
+                    try {
+                        sock = ss.accept();
+                        new AcceptedSocketConnection(sock, m).start();
+                    } catch (IOException e) {
+                        // TODO Auto-generated catch block
+                        e.printStackTrace();
+                    }
+                }
+                logConsole(ChatColor.LIGHT_PURPLE + "-----Done accepting connections-----");
+            }
+        });
+        acceptor.start();
+        super.onEnable();
+    }
+
+    @Override
+    public void onDisable() {
+        acceptorRunning = false;
+        Socket sockCloser;
+        try {
+            sockCloser = new Socket("localhost", listeningport);
+            BufferedWriter out = new BufferedWriter(new OutputStreamWriter(sockCloser.getOutputStream()));
+            out.write(Main.closeConnection);
+            out.close();
+            sockCloser.close();
+            logConsole(ChatColor.DARK_GREEN + "Closed listening web server successfully!");
+        } catch (UnknownHostException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        } catch (IOException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+        try {
+            ss.close();
+        } catch (IOException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+        super.onDisable();
+    }
 }
